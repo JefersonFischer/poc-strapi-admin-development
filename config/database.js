2722e1bedea04a838ee67941543d8a5815c88afd
Jeferson Barbosa Fischer <jeferson.fischer@lojasrenner.com.br>
2022-06-08 11:33:05 -0300

    redeploy

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
-      client: env("DATABASE_CLIENT"),
-  
-      connection: {
-          host: env("DATABASE_HOST"),
-          port: env.int("DATABASE_PORT"),
-          database: env("DATABASE_NAME"),
-          user: env("DATABASE_USERNAME"),
-          password: env("DATABASE_PASSWORD")
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-  debug: false,
+    },
+    debug: false,
   },
-  });
+});
